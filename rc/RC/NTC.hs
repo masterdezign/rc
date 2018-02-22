8bfbb94b3440366bd6b9035c2633229e0b3eb8a4
Bogdan <masterdezign@gmail.com>
2018-02-22 18:34:23 +0100

    Add RC.NTC module

---
+++ rc/RC/NTC.hs
@@ -0,0 +1,7 @@
+-- = Nonlinear transient computing
+--
+-- This module was developed as a part of author's PhD project:
+-- https://www.researchgate.net/project/Theory-and-Modeling-of-Complex-Nonlinear-Delay-Dynamics-Applied-to-Neuromorphic-Computing
+--
+
+module RC.NTC where
